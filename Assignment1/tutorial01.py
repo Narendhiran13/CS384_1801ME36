0dae9cad8e7c7b25a32850c0f8d123052bb2c7fb
Narendhiran13 <narendhiran2000@gmail.com>
2020-09-17 15:11:47 +0530

    Updating the power Function

--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -17,30 +17,49 @@ def multiply(num1, num2):
 # Function to divide two numbers 
 def divide(num1, num2): 
 	#DivisionLogic 
-	division = num1/num2
+	division = num1 /num2
 	return division
 		
 # Function to add power function
-#You cant use the inbuilt python function x ** y . Write your own function
+# You cant use the inbuilt python function x ** y . Write your own function
 def power(num1, num2): #num1 ^ num2
+	if isinstance(num1,int) or isinstance(num1,float):
+		if isinstance(num2,float) or isinstance(num2,int):
+			power = 1 
+			for i in range(num2):
+
+				if num2 == 0: 
+					return 1 
+	
+				power = power * num1
+	
+		else:
+			return "Enter Valid Number"			
+	else:
+		return "Enter Valid Number"			
+	
 	#DivisionLogic 
 	return power
 	
 # Python 3 program to print GP.  geometric Progression
 #You cant use the inbuilt python function. Write your own function
 def printGP(a, r, n): 
-	gp=[]
+	gp=[] 
+
 	return gp 
 
 # Python 3 program to print AP.  arithmetic Progression
 #You cant use the inbuilt python function. Write your own function
 def printAP(a, d, n): 
-	ap=[]
+	ap=[] 
+	
 	return ap
 
 # Python 3 program to print HP.   Harmonic Progression
 #You cant use the inbuilt python function. Write your own function
 def printHP(a, d, n): 
 	hp=[]
+			
 	return hp
+
 
