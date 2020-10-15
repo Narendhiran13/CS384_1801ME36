61f7e93bb262d983b55a2de140f333001e54d5d0
Narendhiran13 <narendhiran2000@gmail.com>
2020-10-15 20:22:43 +0530

    Updated the state Function

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -201,6 +201,31 @@ def dob():
 
 def state():
     # Read csv and process
+
+    Created_directory = "./analytics/state"
+    try:  # Create a Directory if necessary
+        os.makedirs(Created_directory)
+    except:
+        pass
+
+    Heading_Titles = ["id", "full_name", "country", "email",
+                      "gender", "dob", "blood_group", "state"]
+
+    Student_csv = open("studentinfo_cs384.csv", "r")
+    read_dire = csv.DictReader(Student_csv, Heading_Titles)
+
+    for Single_stud_data in read_dire:
+        # Tamil Nadu
+        gender_var = Single_stud_data.get("state").lower()
+
+        File_name = "{}.csv".format(gender_var)
+
+        # open the New File according to the state
+        f = open("{}/{}".format(Created_directory, File_name), "a")
+        # Writing in the File about the Student
+        read_write = csv.DictWriter(f, Heading_Titles)
+        read_write.writerow(Single_stud_data)
+
     pass
 
 
