870e9bc2fa16674fa115ed2cabee5dc11d386f0b
Narendhiran13 <narendhiran2000@gmail.com>
2020-10-15 20:20:15 +0530

    Updated the dob Function

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -160,6 +160,42 @@ def gender():
 
 def dob():
     # Read csv and process
+    Created_directory = "analytics/dob"
+    try:  # Create a Directory if necessary
+        os.makedirs(Created_directory)
+    except:
+        pass
+
+    with open("./studentinfo_cs384.csv", "r") as Student_csv:
+        read_dir = csv.DictReader(Student_csv)
+        fieldname = read_dir.fieldnames
+        next(read_dir, None)
+        Mapping_Var_change = {
+            1: csv.DictWriter(open("{}/bday_1995_1999.csv".format(Created_directory), "w"), fieldname),
+            2: csv.DictWriter(open("{}/bday_2000_2004.csv".format(Created_directory), "w"), fieldname),
+            3: csv.DictWriter(open("{}/bday_2005_2009.csv".format(Created_directory), "w"), fieldname),
+            4: csv.DictWriter(open("{}/bday_2010_2014.csv".format(Created_directory), "w"), fieldname),
+            5: csv.DictWriter(open("{}/bday_2015_2020.csv".format(Created_directory), "w"), fieldname)
+        }
+        for i in range(len(Mapping_Var_change)):
+            Mapping_Var_change.get(i+1).writeheader()
+        for Single_stud_data in read_dir:
+            if re.match(re.compile("[0-9]{2}-[0-9]{2}-[0-9]{4}"), Single_stud_data.get("dob")):
+                year = Single_stud_data.get("dob").split("-")[-1]
+                condition = -1
+                if 1995 <= int(year) <= 1999:
+                    condition = 1
+                elif 2000 <= int(year) <= 2004:
+                    condition = 2
+                elif 2005 <= int(year) <= 2009:
+                    condition = 3
+                elif 2010 <= int(year) <= 2014:
+                    condition = 4
+                elif 2015 <= int(year) <= 2020:
+                    condition = 5
+                if condition != -1:
+                    Mapping_Var_change.get(
+                        condition).writerow(Single_stud_data)
     pass
 
 
