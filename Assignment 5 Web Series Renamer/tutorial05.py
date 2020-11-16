c08b3d59854e3d19c5e3fa1772c15508fb26af95
Narendhiran13 <narendhiran2000@gmail.com>
2020-11-17 00:12:59 +0530

    Update the FIR

--- Assignment 5 Web Series Renamer/tutorial05.py
+++ Assignment 5 Web Series Renamer/tutorial05.py
@@ -90,42 +90,7 @@ def rename_Game_of_Thrones(folder_name):
 
 
 def rename_Sherlock(folder_name):
-    # JOIning the required path where Subtitles & MP4 is Present
-    path = os.path.join(os.getcwd(), os.path.join('Subtitles', folder_name))
-
-    for file in os.listdir(path):  # Checking for Each & every file
-
-        try:
-            # As the Movie Name is present, before '-'.
-            file_name = file.split('.')
-            file_name = [i.strip() for i in file_name]
-
-            # To Extract the name of the Episode
-            if 'E' not in file_name[1]:
-                season = (season_padding -
-                          len(_zero_remove_fn(file_name[1][1:])))*'0' + _zero_remove_fn(file_name[1][1:])
-                episode = (episode_padding -
-                           len(_zero_remove_fn(file_name[2][1:])))*'0' + _zero_remove_fn(file_name[2][1:])
-            else:
-                block = file_name[1].split("E")
-                season = (
-                    season_padding - len(_zero_remove_fn(block[0][1:])))*'0' + _zero_remove_fn(block[0][1:])
-                episode = (episode_padding -
-                           len(_zero_remove_fn(block[1])))*'0' + _zero_remove_fn(block[1])
-
-            # To rename And delete the exisisting old file name.
-            file_name_new = file_name[0] + " - Season " + season + \
-                " Episode " + episode + "." + file_name[-1]
-            file_name_new = os.path.join(path, file_name_new)
-            file_name_old = os.path.join(path, file)
-
-            try:
-                os.rename(file_name_old, file_name_new)
-            except:
-                os.remove(file_name_old)
-        # In case, renaming Fails...
-        except:
-            pass
+    pass
     # rename Logic
 
 
