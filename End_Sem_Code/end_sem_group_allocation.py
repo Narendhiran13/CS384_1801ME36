63ef86969860da9f60f043c3926ce2b56c8cde25
Narendhiran13 <narendhiran2000@gmail.com>
2020-11-30 19:25:41 +0530

    Updating the task3

--- End_Sem_Code/end_sem_group_allocation.py
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -2,7 +2,9 @@ import re
 import csv
 import os
 import pandas as pd
+import shutil
 
+from queue import Queue
 
 average_group_members, average_cs_branch, average_ee_branch, average_mm_branch, average_me_branch, average_cb_branch, average_ce_branch = [
     0 for i in range(7)]
@@ -10,6 +12,39 @@ total_students, mm_branch_strength, cs_branch_strength, ee_branch_strength, me_b
     0 for i in range(7)]
 mm_roll, mm_name, mm_email, me_roll, me_name, me_email, ee_roll, ee_name, ee_email, cb_roll, cb_name, cb_email, ce_roll, ce_name, ce_email, cs_roll, cs_name, cs_email = ([
 ] for i in range(18))
+
+path = './data_file/'
+
+
+def create_or_delete_folder():
+    if os.path.exists("./data_file") == False:
+        os.mkdir("./data_file")
+    else:
+        shutil.rmtree('./data_file')
+        os.mkdir('./data_file')
+
+
+def padding_convert_intostring(value):
+    string_value = str(value)
+    if len(string_value) == 2:
+        return string_value
+    else:
+        return '0'+string_value
+
+
+def branch(roll_no):
+    return_val = ""
+    for ij in roll_no:
+        if ij.isdigit() == False:
+            return_val += ij
+    return return_val
+
+
+def get_number(string):
+    dummy_var = string.split('_')
+    z = dummy_var[1].split('.')
+    return_value = z[0][1:]
+    return return_value
 
 
 def strength_function(Branch, Single_stud_data):
@@ -67,7 +102,7 @@ def task1(number_of_groups):
     global mm_branch_strength, cs_branch_strength, ee_branch_strength, me_branch_strength, cb_branch_strength, ce_branch_strength
     data = [['EE', ee_branch_strength], [
         'ME', me_branch_strength], ['CS', cs_branch_strength], ['CE', ce_branch_strength], ['CB', cb_branch_strength], ['MM', mm_branch_strength]]
-    with open('branch_strength.csv', 'w', newline='') as file:
+    with open(path+'branch_strength.csv', 'w', newline='') as file:
         writer = csv.writer(file, dialect='excel')
         writer.writerow(["BRANCH_CODE", "STRENGTH"])
         writer.writerows(data)
@@ -77,61 +112,106 @@ def task1(number_of_groups):
 def task2():
     data = {"Roll": cs_roll, "Name": cs_name, "Email": cs_email}
     df = pd.DataFrame(data)
-    df.to_csv('CS.csv', index=False, header=True)
+    df.to_csv(path+'CS.csv', index=False, header=True)
 
     data = {"Roll": ce_roll, "Name": ce_name, "Email": ce_email}
     df = pd.DataFrame(data)
-    df.to_csv('CE.csv', index=False, header=True)
+    df.to_csv(path+'CE.csv', index=False, header=True)
 
     data = {"Roll": cb_roll, "Name": cb_name, "Email": cb_email}
     df = pd.DataFrame(data)
-    df.to_csv('CB.csv', index=False, header=True)
+    df.to_csv(path+'CB.csv', index=False, header=True)
 
     data = {"Roll": ee_roll, "Name": ee_name, "Email": ee_email}
     df = pd.DataFrame(data)
-    df.to_csv('EE.csv', index=False, header=True)
+    df.to_csv(path+'EE.csv', index=False, header=True)
 
     data = {"Roll": me_roll, "Name": me_name, "Email": me_email}
     df = pd.DataFrame(data)
-    df.to_csv('ME.csv', index=False, header=True)
+    df.to_csv(path+'ME.csv', index=False, header=True)
 
     data = {"Roll": mm_roll, "Name": mm_name, "Email": mm_email}
     df = pd.DataFrame(data)
-    df.to_csv('MM.csv', index=False, header=True)
+    df.to_csv(path+'MM.csv', index=False, header=True)
 
 
-'''
-def task3(number_of_groups):
-    global mm_branch_strength, cs_branch_strength, ee_branch_strength, me_branch_strength, cb_branch_strength, ce_branch_strength
-    global average_group_members, average_cs_branch, average_ee_branch, average_mm_branch, average_me_branch, average_cb_branch, average_ce_branch
-    print(average_cs_branch, average_ee_branch, average_mm_branch,
-          average_me_branch, average_cb_branch, average_ce_branch)
-    with open('branch_strength.csv', 'w', newline='') as file:
-        writer = csv.writer(file, dialect='excel')
-        writer.writerow(["group", "total", "EE", "ME", "CS", "CE", "CB", "MM"])
-        for n in range(1, number_of_groups+1):
+def sort_fn(pa, number_of_groups):
+    df = pd.read_csv(pa)
+    if os.path.exists('./data_file/stats_grouping.csv') == False:
+        dataframe1 = pd.read_csv('./data_file/branch_strength.csv')
+        data_list = ['group', 'total']
+        for x in dataframe1['BRANCH_CODE']:
+            data_list.append(x)
+        df1 = pd.DataFrame([data_list])
+        file = './data_file/stats_grouping.csv'
+        df1.to_csv(file, mode='a+', index=False, header=False)
+    dataframe1 = pd.read_csv('./data_file/branch_strength.csv')
+    data_list_branch = []
+    for x in dataframe1['BRANCH_CODE']:
+        data_list_branch.append(x)
+    for i in range(1, number_of_groups+1):
+        for j in range(len(df['group'])):
+            x = df.loc[j]
+            grp = x['group']
+            if str(i) in grp and i == int(get_number(grp)):
+                data_list1 = [grp, x['total']]
+                for b in data_list_branch:
+                    data_list1.append(x[b])
+                df_t = pd.DataFrame([data_list1])
+                file = './data_file/stats_grouping.csv'
+                df_t.to_csv(file, mode='a+', index=False, header=False)
 
-            if(average_group_members > (number_of_groups-n+1) * int(average_group_members)):
-                grp_total_mem = int(average_group_members)+1
-            else:
-                grp_total_mem = int(average_group_members)
 
-            if(average_ee_branch > (number_of_groups-n+1) * int(average_ee_branch)):
-                ee_grp_total_mem = int(average_ee_branch)+1
-            else:
-                ee_grp_total_mem = int(average_ee_branch)
+def task4(filename, number_of_groups):
+    files = os.listdir('./data_file')
+    for single_file in files:
+        if 'Group' in single_file:
+            if os.path.exists('./data_file/stats grouping.csv') == False:
+                data_list = ['group', 'total']
+                dataframe1 = pd.read_csv('./data_file/branch_strength.csv')
 
-            if(average_ee_branch > (number_of_groups-n+1) * int(average_ee_branch)):
-                ee_grp_total_mem = int(average_ee_branch)+1
-            else:
-                ee_grp_total_mem = int(average_ee_branch)
+                for x in dataframe1['BRANCH_CODE']:
+                    data_list.append(x)
+                df1 = pd.DataFrame([data_list])
+                file_temp = './data_file/stats grouping.csv'
+                df1.to_csv(file_temp, mode='a+', index=False, header=False)
 
-            for ee_grp + me_grp + cs_grp+ce_grp+cb_grp+mm_grp == grp_total_mem:
-'''
+            dict1 = {}
+            csv_file = './data_file/'+single_file
+            df2 = pd.read_csv(csv_file)
+            total = len(df2['Roll'])
+            for roll_no_val in df2['Roll']:
+                branch_data_1 = branch(roll_no_val)
+                if branch_data_1 in dict1:
+                    dict1[branch_data_1] += 1
+                else:
+                    dict1[branch_data_1] = 1
+
+            data_list_2 = [single_file, total]
+
+            dataframe1 = pd.read_csv('./data_file/branch_strength.csv')
+            for x in dataframe1['BRANCH_CODE']:
+                data_list_2.append(dict1[x])
+            df_7 = pd.DataFrame([data_list_2])
+            file_temp1 = './data_file/'+'stats grouping.csv'
+            df_7.to_csv(file_temp1, mode='a+', index=False, header=False)
+
+    df_8 = pd.read_csv('./data_file/stats grouping.csv')
+    df_8.sort_index(ascending=True, inplace=True)
+    data_list = ['group', 'total']
+    dataframe1 = pd.read_csv('./data_file/branch_strength.csv')
+    for x in dataframe1['BRANCH_CODE']:
+        data_list.append(x)
+    df_te1 = pd.DataFrame([data_list])
+    f1 = './data_file/stats grouping.csv'
+    df_te1.to_csv(f1, mode='w', index=False, header=False)
+    df_8.to_csv(f1, mode='a+', index=False, header=False)
+    sort_fn('./data_file/stats grouping.csv', number_of_groups)
 
 
 def group_allocation(filename, number_of_groups):
-
+    create_or_delete_folder()
+    ind_value = 1
     Order_followed = re.compile("[0-9]{4}[a-z]{2}[0-9]{2}", re.I)
 
     Heading_Title = ["Roll", "Name", "Email"]
@@ -154,6 +234,52 @@ def group_allocation(filename, number_of_groups):
         print("\nInvalid 'Number of Groups' value given, kindly change accordingly!\n\n\t0 < number_of_groups < total_students\n")
         return
 
+    df = pd.read_csv('./data_file/branch_strength.csv')
+    queque_use = Queue()
+
+    for no_of_group in range(1, number_of_groups+1):
+        group_file = 'Group_G'+padding_convert_intostring(no_of_group)+'.csv'
+        for ij in range(len(df['BRANCH_CODE'])):
+            branch_code = df.loc[ij]
+            branch_strength = branch_code[1]
+            average_stud_perGrp = branch_strength//number_of_groups
+            file = './data_file/'+branch_code[0]+'.csv'
+            df1 = pd.read_csv(file)
+            for index in range(average_stud_perGrp*(no_of_group-1), average_stud_perGrp*no_of_group):
+                student = df1.loc[index]
+                path = './data_file/'+group_file
+                if os.path.exists(path) == False:
+                    df_un_new = pd.DataFrame([['Roll', 'Name', 'Email']])
+                    df_un_new.to_csv(
+                        path, mode='a+', index=False, header=False)
+                df_new = pd.DataFrame(
+                    [[student['Roll'], student['Name'], student['Email']]])
+                df_new.to_csv(path, mode='a+', index=False, header=False)
+
+    for i in range(len(df['BRANCH_CODE'])):
+        stud_data_x = df.loc[i]
+        f = './data_file/'+stud_data_x[0]+'.csv'
+        dataframe = pd.read_csv(f)
+        average_stud_perGrp = stud_data_x[1]//number_of_groups
+        for jk in range(average_stud_perGrp*number_of_groups, stud_data_x[1]):
+            stud_data = dataframe.loc[jk]
+            queque_use.put(
+                (stud_data['Roll'], stud_data['Name'], stud_data['Email']))
+
+    while queque_use.empty() == False:
+        if ind_value > number_of_groups:
+            ind_value = 1
+        group_file1 = 'Group_G'+padding_convert_intostring(ind_value)+'.csv'
+        path_value = './data_file/'+group_file1
+
+        x_value = queque_use.get()
+        df_3 = pd.DataFrame([[x_value[0], x_value[1], x_value[2]]])
+        df_3.to_csv(path_value, mode='a+', index=False, header=False)
+        ind_value += 1
+
+    task4(filename, number_of_groups)
+    os.remove('./data_file/stats grouping.csv')
+
     average_function(total_students, mm_branch_strength, cs_branch_strength,
                      ee_branch_strength, me_branch_strength, cb_branch_strength, ce_branch_strength)
 
@@ -162,6 +288,5 @@ def group_allocation(filename, number_of_groups):
 
 
 filename = "Btech_2020_master_data.csv"
-number_of_groups = 12
+number_of_groups = int(input("Enter the number of groups\n"))
 group_allocation(filename, number_of_groups)
-print(average_group_members)
